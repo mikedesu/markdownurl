7a228b3d1193796f65d0104a9f867ba1e98677f0
Mike <ezuratechan@gmail.com>
2017-07-05 21:56:52 -0400

    converting HTML entities to ASCII

--- markdownurl.sh
+++ markdownurl.sh
@@ -1,4 +1,4 @@
 #!/opt/local/bin/bash
-TESTVAR=$(wget -qO- $1 | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si')
+TESTVAR=$(wget -qO- $1 | perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' | recode html..ascii)
 echo "[$TESTVAR]($1)"
 
